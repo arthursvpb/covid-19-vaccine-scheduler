ae17a1e38e3b583a05b0a3aba023ebf051c69af7
arthursvpb <arthurandroid15@gmail.com>
2021-04-21 16:25:05 -0300

    start testing with react testing library

---
+++ web/src/__tests__/App.test.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import React from 'react';
+
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('renders correctly', () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
+});
