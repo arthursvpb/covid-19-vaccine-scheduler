8f269e040b7dddc7020155ce9d3a65578d2e6bb6
arthursvpb <arthurandroid15@gmail.com>
2021-04-22 14:24:45 -0300

    change baseUrl from client

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const { REACT_APP_DEV_API_BASE_URL: baseURL } = process.env;
+const { REACT_APP_API_BASE_URL: baseURL } = process.env;
 
 const api = axios.create({
   baseURL,
